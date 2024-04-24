3cdf227523901a59077fe963bc47bb14e1f26f5d
nguyenhoangnhan2003 <140692347+nguyenhoangnhan2003@users.noreply.github.com>
2024-04-24 12:41:41 +0700

    update

--- src/Backend/src/server.js
+++ src/Backend/src/server.js
@@ -4,5 +4,7 @@ const app = express()
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
-
+app.get('/fdgdfg', function (req, res) {
+    res.send('Hello World')
+})
 app.listen(3000)
