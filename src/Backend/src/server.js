539437c18d929ac05a97df10d4fcec1de2923edf
unknown <nhnhan2102@gmail.com>
2024-04-29 23:21:36 +0700

    update

--- src/Backend/src/server.js
+++ src/Backend/src/server.js
@@ -1,9 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
+require('dotenv').config();
+const configViewEngine = require('./config/viewEngine.js');
 
 //configViewEngine
 configViewEngine(app);
 
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
+
+//config json api
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 //used router
 app.use('/', webRoutes);
